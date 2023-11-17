60188a8f3253238721b551ed110273dccd4969ca
Jeshua Maxey <me@jeshua.co>
2023-11-17 10:50:48 +0000

    rm boiler plate. add splash screen

---
+++ components/splash-screen.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { motion, AnimatePresence, animate } from "framer-motion"
+
+import { AspectRatio } from "./ui/aspect-ratio"
+import { Button } from "./ui/button"
+
+const AnimateOutWrapper = ({
+  children, visible, customMotionProps
+}: {
+  children: React.ReactNode, visible: boolean, customMotionProps?: {}
+}) => {
+  const defaultMotionProps = {
+    layout: true,
+    initial: false,
+    animate: { opacity: 1, stagger: .100 },
+    exit: { opacity: 0, y: 40 },
+    transition: { ease: "easeOut", duration: .400 },
+  }
+
+  const motionProps = {
+    ...defaultMotionProps,
+    ...customMotionProps
+  }
+
+  console.log({motionProps})
+  
+  return (
+    <AnimatePresence>
+      {visible && (
+        <motion.div {...motionProps}>
+          {children}
+          </motion.div>
+        )}
+    </AnimatePresence>
+  )
+}
+
+const SplashScreen = () => {
+  const router = useRouter()
+  const [startingGame, setStartingGame] = useState(false)
+
+  const startGame = () => {
+    setStartingGame(true)
+    setTimeout(() => {
+    router.push('/game')
+    }, 2000)
+  }
+  
+  return (
+    <div className="w-full flex flex-col gap-20 items-center">
+      <AnimateOutWrapper visible={!startingGame}>
+        <div className="text-center">
+          <h1 className="text-3xl leading-loose">Identify 3 F1 circuits</h1>
+          <h2 className="text-3xl leading-loose">Fastest wins</h2>
+          <h3 className="text-xl leading-loose">New game daily</h3>
+        </div>
+      </AnimateOutWrapper>
+      
+      <div className="w-full h-64 px-8">
+        <AnimateOutWrapper visible={!startingGame} customMotionProps={{
+          exit: { transform: "scale(4)"},
+          transition: { ease: "easeOut", duration: 1.500 }
+        }}>
+          <AspectRatio ratio={16 / 9}>
+            <div className="w-full h-full flex bg-slate-100 bg-foreground/50">
+              <p className="w-full text-center m-auto">
+                game preview here
+              </p>
+            </div>
+          </AspectRatio>
+        </AnimateOutWrapper>
+      </div>
+
+      <AnimateOutWrapper visible={!startingGame}>
+        <div>
+          <Button onClick={startGame}>Play</Button>
+        </div>
+      </AnimateOutWrapper>
+    </div>
+  )
+}
+
+export default SplashScreen
