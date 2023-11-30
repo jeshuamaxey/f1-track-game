29500111aa411b0f0c7394236e5a6075902895f2
Jeshua Maxey <me@jeshua.co>
2023-11-30 11:25:36 +0000

    separate login and sign up

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,30 +4,41 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 
-export default function Login({
+export default function SignUp({
   searchParams,
 }: {
-  searchParams: { errorMessage: string, emailDomain: string }
+  searchParams: { errorMessage: string, postSignUpMessage: string, emailDomain: string }
 }) {
-  const signIn = async (formData: FormData) => {
+  const signUp = async (formData: FormData) => {
     'use server'
 
+    const origin = headers().get('origin')
     const email = formData.get('email') as string
     const password = formData.get('password') as string
     const cookieStore = cookies()
     const supabase = createClient(cookieStore)
 
-    const { error } = await supabase.auth.signInWithPassword({
+    const { error } = await supabase.auth.signUp({
       email,
       password,
+      options: {
+        emailRedirectTo: `${origin}/auth/callback?postSignUp=1`,
+      },
     })
 
     if (error) {
-      return redirect('/login?errorMessage=Could not authenticate user')
+      if(error.message === "User already registered") {
+        return redirect('/signup?errorMessage=Email already in use. Try signing in')
+      } else {
+        return redirect('/signup?errorMessage=Could not create a user profile')
+      }
     }
 
-    return redirect('/processing')
+    const domain = email.split('@')[1]
+    return redirect(`/signup?postSignUpMessage=We have sent you an email to verify your profile. Please check your email (including your spam) to continue.&emailDomain=${domain}`)
   }
+
+  const showForm = !searchParams.postSignUpMessage
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
@@ -52,12 +63,11 @@ export default function Login({
         Back
       </Link>
 
-      <form
+      {showForm && <form
         className="animate-in flex-1 flex flex-col w-full justify-center gap-2"
-        action={signIn}
+        action={signUp}
       >
-        <h1 className="text-3xl pb-4 text-center">Login</h1>
-        <p className="pb-8 text-center">Login to access your profile and stats</p>
+        <p className="pb-8 text-center">Track your progress by creating an account</p>
         <label className="text-md" htmlFor="email">
           Email
         </label>
@@ -78,18 +88,23 @@ export default function Login({
           required
         />
         <Button className="mb-2">
-          Log In
+          Sign Up
         </Button>
-
-        <p className="text-center">Or <Link className="underline" href="/signup">create an account here</Link>.</p>
-
+        <p className="text-center">Got an account? <Link className="underline" href="/login">Login here</Link></p>
         {searchParams?.errorMessage && (
           <p className="mt-4 p-4 bg-foreground/10 text-center border-l-4 border-red-500">
             {searchParams.errorMessage}
           </p>
         )}
       </form>
+      }
 
+      {/* POST SIGNUP UI */}
+      {searchParams?.postSignUpMessage && (
+        <p className="my-4 p-4 bg-foreground/10 text-center border-l-4 border-slate-600">
+          {searchParams.postSignUpMessage}
+        </p>
+      )}
     </div>
   )
 }
